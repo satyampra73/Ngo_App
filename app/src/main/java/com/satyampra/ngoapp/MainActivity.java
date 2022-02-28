d5f17b76f0092b69adc0cd2c46581ec959ac753d
Satyam Prajapati <satyam7355pra@gmail.com>
2022-02-28 09:05:37 +0530

    initial commit by satyam

---
+++ app/src/main/java/com/satyampra/ngoapp/MainActivity.java
@@ -0,0 +1,20 @@
+package com.satyampra.ngoapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void goToNew(View view) {
+        startActivity(new Intent(MainActivity.this,RegisterActivity.class));
+    }
+}
---
+++ app/src/main/java/com/satyampra/ngoapp/RegisterActivity.java
@@ -0,0 +1,18 @@
+package com.satyampra.ngoapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+
+    public void Signin(View view) {
+    }
+}
